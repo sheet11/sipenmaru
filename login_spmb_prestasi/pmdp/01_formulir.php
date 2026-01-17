97bcf1f90ceb719580afc006843e957ca7e2d46f
sheet11 <ahmadreynaldo@gmail.com>
2026-01-17 13:47:25 +0700

    formulir

--- login_spmb_prestasi/pmdp/01_formulir.php
+++ login_spmb_prestasi/pmdp/01_formulir.php
@@ -1,27 +1,32 @@
-<?php 
-	include"01_nav.php";
-	error_reporting(0);
-	include "../config/koneksi.php"; 
+<?php
+include "01_nav.php";
+error_reporting(0);
+include "../config/koneksi.php";
 ?>
 
 <div id="page-wrapper">
 	<div id="page-inner">
-		<h2>Selamat Datang</h2>   
-	    <h5>Di Halaman Sistem Informasi Pendaftaran Mahasiswa Baru Online</h5>
-		<hr></hr>
+		<h2>Selamat Datang</h2>
+		<h5>Di Halaman Sistem Informasi Pendaftaran Mahasiswa Baru Online</h5>
+		<hr>
+		</hr>
 		<div>
-            <table class="table table-bordered table-striped">
-                <thead>			
-		  			<tr class="info">
-						<th width="5%">No.</th><th>ID </th><th>Nama Lengkap </th><th>Prodi</th><th width="25%">Aksi</th>
+			<table class="table table-bordered table-striped">
+				<thead>
+					<tr class="info">
+						<th width="5%">No.</th>
+						<th>ID </th>
+						<th>Nama Lengkap </th>
+						<th>Prodi</th>
+						<th width="25%">Aksi</th>
 					</tr>
 				</thead>
-			<?php 
-					$query=mysqli_query($kon,"select * from tb_formulir3 where username='$_SESSION[username]' ");
-				
-					$i =  +1;		
-				while($a=mysqli_fetch_array($query)){
-				echo"
+				<?php
+				$query = mysqli_query($kon, "select * from tb_formulir3 where username='$_SESSION[username]' ");
+
+				$i =  +1;
+				while ($a = mysqli_fetch_array($query)) {
+					echo "
 				<tr>
 					<td>$i</td>
 					<td>$a[username]</td>
@@ -31,36 +36,32 @@
 					
 					<td>";
 
-						echo "
+					echo "
 						
-												
+						<a href='01_edit_formulir.php?id_formulir=$a[id_formulir]' class='btn btn-danger'><b>Edit</b></a>					
 						<a href='01_lihat_formulir.php?id_formulir=$a[id_formulir]' class='btn btn-primary'><b>Lihat</b></a>
 						
 						
 						 ";
-	
 
-			
-				echo"	</td>
+
+
+					echo "	</td>
 					</tr>
 					</table>";
 
 
-				$i++;
-			}
-			?>
-			<!-- <a href='01_edit_formulir.php?id_formulir=$a[id_formulir]' class='btn btn-danger'><b>Edit</b></a> -->
-			<tr>
-				<td><b>Catatan</b><br>
-				1. Klik Tombol Edit Untuk Melengkapi Data</br>
-				</td>
-			</tr>
+					$i++;
+				}
+				?>
+				<!-- <a href='01_edit_formulir.php?id_formulir=$a[id_formulir]' class='btn btn-danger'><b>Edit</b></a> -->
+				<tr>
+					<td><b>Catatan</b><br>
+						1. Klik Tombol Edit Untuk Melengkapi Data</br>
+					</td>
+				</tr>
 			</table>
-		
-	 
+
+
+		</div>
 	</div>
-</div>
-
-
-
-
