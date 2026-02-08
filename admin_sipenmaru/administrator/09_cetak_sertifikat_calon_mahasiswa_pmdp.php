1ebcbedb3411477b21605817a32b9bce962f5c2f
sheet <ahmadreynaldo@gmail.com>
2026-02-08 09:51:06 +0700

    edit cetak sertifikat

--- admin_sipenmaru/administrator/09_cetak_sertifikat_calon_mahasiswa_pmdp.php
+++ admin_sipenmaru/administrator/09_cetak_sertifikat_calon_mahasiswa_pmdp.php
@@ -22,57 +22,202 @@
 
 ?>
         
-        <table border=1>
-          <thead>
-            <th>No.</th>
-            <th>Username</th>
-            <th>Nama Prestasi</th>
-            <th>Keterangan</th>
-            <th>Tingkat</th>
-            <th>Bukti</th>
-          </thead>
-          <?php 
-          $i = 1;
-            $auery=mysqli_query($kon,"select * from tb_prestasi where username='$_GET[username]'");
-            while($a=mysqli_fetch_array($auery)){
-          ?>
-          <tr>
-            <td><?= $i++ ?></td>
-            <td><?php echo $a['username'];?></td>
-            <td><?php echo $a['nama'];?></td>
-            <td><?php echo $a['ket'];?></td>
-            <td><?php 
-							if($a['tingkat']==1)
-                    {
-                        $nprestasi='Internasional';
-                    }
-                    elseif($a['tingkat']==2)
-                    {
-                        $nprestasi='Nasional';
-                    }
-                    elseif($a['tingkat']==3)
-                    {
-                        $nprestasi='Regional';
-                    }
-                    elseif($a['tingkat']==4)
-                    {
-                        $nprestasi='Provinsi';
-                    }
-                    elseif($a['tingkat']==5)
-                    {
-                        $nprestasi='Kabupaten/Kota';
-                    }
-                    elseif($a['tingkat']==6)
-                    {
-                        $nprestasi='Sekolah';
-                    }echo $nprestasi;?></td>
-            <td>
-            <a href="../../login_spmb_prestasi/pmdp/prestasi/<?php echo $a['bukti_n'];?>" class="fancy">
-            <img src="../../login_spmb_prestasi/pmdp/prestasi/<?php echo $a['bukti_n'];?>" alt="" width=150 height=175 border="0" /></a></td>
-            </tr>
-            <?php } ?>
-            </thead>
-        </table>
+        <?php
+$qt = mysqli_query($kon, "
+  SELECT * FROM tb_prestasi
+  WHERE username = '$_GET[username]'
+  AND (
+        jenis_prestasi = 'Prestasi Akademik'
+        OR jenis_prestasi = 'Prestasi Non Akademik'
+      )
+  ORDER BY tgl DESC
+");
+
+if (mysqli_num_rows($qt) > 0) {
+?>
+<h2 style="text-align:center; color:black;">Prestasi Akademik & Non Akademik</h2>
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th>No</th>
+      <th>Nama</th>
+      <th>Tingkat</th>
+      <th>Tanggal</th>
+      <th>Bukti</th>
+      <th>Keterangan</th>
+      <th>Aksi</th>
+    </tr>
+  </thead>
+  <?php $no=1; while($r=mysqli_fetch_assoc($qt)){ ?>
+  <tr>
+    <td><?= $no++; ?></td>
+    <td><?= htmlspecialchars($r['nama']); ?></td>
+    <td><?= $tingkatText[$r['tingkat']] ?? '-'; ?></td>
+    <td><?= $r['tgl']; ?></td>
+    <td><img src="prestasi/<?= $r['bukti_n']; ?>" width="130"></td>
+    <td><?= htmlspecialchars($r['ket']); ?></td>
+    <td>
+      <button onclick="konfirmasiHapus(<?= $r['id']; ?>, '<?= htmlspecialchars($r['nama'], ENT_QUOTES); ?>')" 
+              class="btn btn-danger btn-sm">
+        <i class="fa fa-trash"></i> Hapus
+      </button>
+    </td>
+  </tr>
+  <?php } ?>
+</table>
+<?php } ?>
+        
+<?php
+$qt = mysqli_query($kon, "
+  SELECT * FROM tb_prestasi 
+  WHERE username='$_SESSION[username]' 
+  AND jenis_prestasi = 'Prestasi Akademik (Juara Kelas)'
+  ORDER BY tgl DESC
+");
+
+if (mysqli_num_rows($qt) > 0) {
+?>
+<h2 style="text-align:center; color:black;">Prestasi Juara Kelas</h2>
+
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th>No</th>
+      <th>Nama</th>
+      <th>Tingkat</th>
+      <th>Tanggal</th>
+      <th>Bukti</th>
+      <th>Keterangan</th>
+      <th>Aksi</th>
+    </tr>
+  </thead>
+
+  <?php $no=1; while($r=mysqli_fetch_assoc($qt)){ ?>
+  <tr>
+    <td><?= $no++; ?></td>
+    <td><?= htmlspecialchars($r['nama'] ?? 'Juara Kelas / Sekolah'); ?></td>
+    <td><?= $tingkatText[$r['tingkat']] ?? 'Sekolah'; ?></td>
+    <td><?= $r['tgl']; ?></td>
+    <td><img src="prestasi/<?= $r['bukti_n']; ?>" width="130"></td>
+    <td><?= htmlspecialchars($r['ket']); ?></td>
+    <td>
+      <button onclick="konfirmasiHapus(<?= $r['id']; ?>, 'Juara Kelas')" 
+              class="btn btn-danger btn-sm">
+        <i class="fa fa-trash"></i> Hapus
+      </button>
+    </td>
+  </tr>
+  <?php } ?>
+
+</table>
+<?php } ?>
+
+<?php
+// Tahfiz
+$qt = mysqli_query($kon, "
+  SELECT * FROM tb_prestasi
+  WHERE username='$_SESSION[username]' AND jenis_prestasi='Tahfiz' AND juz IS NOT NULL AND juz<>'' ORDER BY tgl DESC
+");
+if (mysqli_num_rows($qt)>0) {
+?>
+<h2 style="text-align:center; color:black;">Prestasi Tahfiz</h2>
+<table class="table table-bordered">
+  <thead>
+  <tr>
+    <th>No</th>
+    <!-- <th>Tingkat</th> -->
+    <th>Tanggal</th>
+    <th>Bukti</th>
+    <th>Juz</th>
+    <th>Aksi</th>
+  </tr>
+</thead>
+<?php $no=1; while($r=mysqli_fetch_assoc($qt)){ ?>
+<tr>
+  <td><?= $no++; ?></td>
+  <!-- <td><?= $tingkatText[$r['tingkat']] ?? '-'; ?></td> -->
+  <td><?= $r['tgl']; ?></td>
+  <td><a href="prestasi/<?= $r['bukti_n']; ?>" target="_blank"><img src="prestasi/<?= $r['bukti_n']; ?>" width="130"></a></td>
+  <td><?= htmlspecialchars($r['juz']); ?></td>
+  <td>
+    <button onclick="konfirmasiHapus(<?= $r['id']; ?>, 'Tahfiz <?= htmlspecialchars($r['juz']); ?>')" 
+            class="btn btn-danger btn-sm">
+      <i class="fa fa-trash"></i> Hapus
+    </button>
+  </td>
+</tr>
+<?php } ?>
+</table>
+<?php } ?>
+
+<?php
+// Paskibraka
+$qt = mysqli_query($kon, "SELECT * FROM tb_prestasi WHERE username='$_SESSION[username]' AND jenis_prestasi='Paskibraka' ORDER BY tgl DESC");
+if (mysqli_num_rows($qt)>0) {
+?>
+<h2 style="text-align:center; color:black;">Paskibraka</h2>
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th>No</th>
+      <th>Tingkat</th>
+      <th>Tanggal</th>
+      <th>Bukti</th>
+      <th>Aksi</th>
+    </tr>
+  </thead>
+<?php $no=1; while($r=mysqli_fetch_assoc($qt)){ ?>
+<tr>
+  <td><?= $no++; ?></td>
+  <td><?= $tingkatText[$r['tingkat']] ?? '-'; ?></td>
+  <td><?= $r['tgl']; ?></td>
+  <td><a href="prestasi/<?= $r['bukti_n']; ?>" target="_blank"><img src="prestasi/<?= $r['bukti_n']; ?>" width="130"></a></td>
+  <td>
+    <button onclick="konfirmasiHapus(<?= $r['id']; ?>, 'Paskibraka')" 
+            class="btn btn-danger btn-sm">
+      <i class="fa fa-trash"></i> Hapus
+    </button>
+  </td>
+</tr>
+<?php } ?>
+</table>
+<?php } ?>
+
+<?php
+// Kemampuan Berbahasa Asing
+$qt = mysqli_query($kon, "SELECT * FROM tb_prestasi WHERE username='$_SESSION[username]' AND jenis_prestasi='Kemampuan Berbahasa Asing' ORDER BY tgl DESC");
+if (mysqli_num_rows($qt)>0) {
+?>
+<h2 style="text-align:center; color:black;">Kemampuan Bahasa Asing</h2>
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th>No</th>
+      <th>Bahasa</th>
+      <th>Level/Nilai</th>
+      <th>Tanggal</th>
+      <th>Bukti</th>
+      <th>Aksi</th>
+    </tr>
+  </thead>
+<?php $no=1; while($r=mysqli_fetch_assoc($qt)){ ?>
+<tr>
+  <td><?= $no++; ?></td>
+  <td><?= htmlspecialchars($r['bahasa']); ?></td>
+  <td><?= htmlspecialchars($r['ket']); ?></td>
+  <td><?= $r['tgl']; ?></td>
+  <td><a href="prestasi/<?= $r['bukti_n']; ?>" target="_blank"><img src="prestasi/<?= $r['bukti_n']; ?>" width="130"></a></td>
+  <td>
+    <button onclick="konfirmasiHapus(<?= $r['id']; ?>, 'Bahasa <?= htmlspecialchars($r['bahasa']); ?>')" 
+            class="btn btn-danger btn-sm">
+      <i class="fa fa-trash"></i> Hapus
+    </button>
+  </td>
+</tr>
+<?php } ?>
+</table>
+<?php } ?>
+
       </div>
     </div>
   </div>
