8a0eb3eeb311de8112293ec7e5eee971b83e08df
sheet11 <ahmadreynaldo@gmail.com>
2026-02-28 09:12:43 +0700

    up buka tutup login

--- login_spmb_prestasi/index.php
+++ login_spmb_prestasi/index.php
@@ -1,36 +1,48 @@
 <!DOCTYPE html>
-<html >
-  <head>
-    <meta charset="UTF-8">
-    <title>Login form</title>
-    <link href="assets/css/font-awesome.css" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/reset.css">
-    <link rel="stylesheet" href="assets/css/stylelogin.css">
-    <script src="js/prefixfree.min.js"></script>
+<html>
 
-  </head>
+<head>
+  <meta charset="UTF-8">
+  <title>Login form</title>
+  <link href="assets/css/font-awesome.css" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/reset.css">
+  <link rel="stylesheet" href="assets/css/stylelogin.css">
+  <script src="js/prefixfree.min.js"></script>
 
-  <body><!--/  Form Login -->
-        <div class="login">
-          <h1>SPMB Online </h1>
-              <form class="form" action="proseslogin.php" method="post" enctype="multipart/form-data">
-                  <p class="field">
-                    <input type="text" name="username" placeholder="ID " required/>
-                    <i class="fa fa-user"></i>
-                  </p>
+</head>
 
-                  <p class="field">
-                    <input type="password" name="password" placeholder="Password / Pin" required/>
-                    <i class="fa fa-lock"></i>
-                  </p>
-                <p class="submit"><input type="submit" name="sent" value="Login"></p>
-              </form>
-        </div> <!--/ Login-->
+<body><!--/  Form Login -->
+  <?php
+  include "config/koneksi.php";
+  $sql = mysqli_query($kon, "select * from periode where nama_periode='Login Prestasi' and status='Buka'");
+  $a = mysqli_fetch_array($sql);
+  if ($a['status'] == "Buka") { ?>
+    <div class="login">
+      <h1>SPMB Online </h1>
+      <form class="form" action="proseslogin.php" method="post" enctype="multipart/form-data">
+        <p class="field">
+          <input type="text" name="username" placeholder="ID " required />
+          <i class="fa fa-user"></i>
+        </p>
 
-<div class="copyright">
-    <p>Seleksi Pendaftaran Mahasiswa Baru Jalur Prestasi </a></p>
-    <p>Copyright &copy; Poltekkes Kemenkes Bengkulu </a></p>
-</div>
-      
-  </body>
+        <p class="field">
+          <input type="password" name="password" placeholder="Password / Pin" required />
+          <i class="fa fa-lock"></i>
+        </p>
+        <p class="submit"><input type="submit" name="sent" value="Login"></p>
+      </form>
+    </div> <!--/ Login-->
+
+    <div class="copyright">
+      <p>Seleksi Pendaftaran Mahasiswa Baru Jalur Prestasi </a></p>
+      <p>Copyright &copy; Poltekkes Kemenkes Bengkulu </a></p>
+    </div>
+  <?php } else {
+    echo "<script>alert('Maaf, Login Jalur Prestasi Sedang Ditutup');window.location='" . $_SERVER['HTTP_REFERER'] . "'</script>";
+  }
+  ?>
+
+
+</body>
+
 </html>
