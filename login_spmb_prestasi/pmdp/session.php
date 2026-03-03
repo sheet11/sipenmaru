7cedc133da23477e7fbf7b79dfc46389eab5d3ec
sheet <ahmadreynaldo@gmail.com>
2026-03-03 09:47:19 +0700

    sesiion 2

--- login_spmb_prestasi/pmdp/session.php
+++ login_spmb_prestasi/pmdp/session.php
@@ -4,6 +4,6 @@ session_start();
 //jika session username belum dibuat, atau session username kosong
 if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
 	//redirect ke halaman login
-	header('location:../index.php');
+	echo "<script>alert('sesi habis, silahkan login kembali');window.location='../index.php'</script>";
 }
 ?>
