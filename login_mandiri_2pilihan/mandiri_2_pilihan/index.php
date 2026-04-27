5fcf0f9ce3f97689a5ed6c93b64e42beb8d667eb
sheet <ahmadreynaldo@gmail.com>
2026-04-27 08:52:20 +0700

    update

--- login_mandiri_2pilihan/mandiri_2_pilihan/index.php
+++ login_mandiri_2pilihan/mandiri_2_pilihan/index.php
@@ -6,19 +6,21 @@ include "01_nav.php";
 error_reporting(0);
 include "../config/koneksi.php"; 
 ?>
+
 <body>
-    <div id="page-wrapper" >
-            <div id="page-inner">
-                <div class="row">
-                    <div class="col-md-12">
-                     <h2>Poltekkes Kemenkes Bengkulu</h2>   
-                        <h5>Selamat Datang <b><?php echo $_SESSION['nama_lengkap'];?> </b> Di Halaman Seleksi Penerimaan Mahasiswa Baru <b><?php echo $_SESSION['level'];?></b></h5>
-                        <br>
-                        <div style="margin-top: 20px; padding: 15px; border: 1px solid #ccc; background-color: #f9f9f9; border-radius: 5px;">
+    <div id="page-wrapper">
+        <div id="page-inner">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Poltekkes Kemenkes Bengkulu</h2>
+                    <h5>Selamat Datang <b><?php echo $_SESSION['nama_lengkap'];?> </b> Di Halaman Seleksi Penerimaan
+                        Mahasiswa Baru <b><?php echo $_SESSION['level'];?></b></h5>
+                    <br>
+                    <!-- <div style="margin-top: 20px; padding: 15px; border: 1px solid #ccc; background-color: #f9f9f9; border-radius: 5px;">
                         <strong>📢 Pengumuman:</strong>
                             <p>Tanggal Ujian Sudah Dekat, Dimohon untuk peserta segera mencetak Kartu Ujian sebagai persyaratan ujian nanti.</p>
-                        </div>
-                        <!-- <?php 
+                        </div> -->
+                    <!-- <?php 
                         $data = mysqli_query($kon,"SELECT * FROM tb_formulir4 WHERE username='$_SESSION[username]'");
                         $a = mysqli_fetch_array($data);
                         
@@ -27,14 +29,14 @@ include "../config/koneksi.php";
                         <?php }elseif($a['status_lulus'] == 'Lulus' || $a['status_lulus'] == 'Tidak Lulus'){ ?> 
                         <h1><a href="cetak_pengumuman.php">Lihat Kelulusan</a></h1>
                         <?php }else{} ?> -->
-                    </div>
-                </div> 
-                             
-        
-             </div>
-             <div>
-                 <!--<a href="cetak_pengumuman.php">Lihat Kelulusan</a>-->
-             </div>
-             </div>
-            </div>   
-           </body>
+                </div>
+            </div>
+
+
+        </div>
+        <div>
+            <!--<a href="cetak_pengumuman.php">Lihat Kelulusan</a>-->
+        </div>
+    </div>
+    </div>
+</body>
