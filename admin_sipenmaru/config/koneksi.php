5de486bb0acd92c4a8edd1ab036be6f3af8f04be
sheet11 <ahmadreynaldo@gmail.com>
2026-05-12 22:44:28 +0700

    as

--- admin_sipenmaru/config/koneksi.php
+++ admin_sipenmaru/config/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$kon=mysqli_connect("localhost","u128202965_sipenmaru","Bengkulu2024$","u128202965_sipenmaru");
+$kon=mysqli_connect("153.92.15.9","u128202965_sipenmaru","Bengkulu2024$","u128202965_sipenmaru");
 echo mysqli_error($kon);
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
