83546c4bd50ce9375ef89ca4dedfb6129e5201d8
sheet <ahmadreynaldo@gmail.com>
2026-07-23 15:35:51 +0700

    feat: add report pages for parental occupation statistics by study program

--- admin_sipenmaru/administrator/01_laporan1pil_pekerjaan.php
+++ admin_sipenmaru/administrator/01_laporan1pil_pekerjaan.php
@@ -16,6 +16,8 @@
         "Lainnya"    => ["Lainnya", ""],
     ];
 
+    $chart_data_ibu = [];
+
     $prodi_list = [
         "Keseluruhan" => "",
         "RPL Sarjana Terapan Gizi" => "RPL Sarjana Terapan Gizi",
@@ -107,10 +109,54 @@
                             $query = mysqli_query($kon, "SELECT 1 FROM tb_formulir5 WHERE pilihan_prodi = '$prodi_value' AND (status='Sudah Membayar' OR status='Terdaftar') AND pekerjaan_orang_tua_ibu IN ($in_values) AND tahun_pendaftaran='2026'");
                         }
                         $jumlah = mysqli_num_rows($query);
+
+                        // Simpan total baris "Keseluruhan" untuk data grafik di bawah tabel
+                        if ($prodi_value === "") {
+                            $chart_data_ibu[$pekerjaan_label] = $jumlah;
+                        }
                     ?>
                         <td><?= $jumlah ?></td>
                     <?php endforeach; ?>
                 </tr>
                 <?php endforeach; ?>
             </table>
+
+            <div class="row">
+                <div class="col-md-8">
+                    <canvas id="chartPekerjaanIbu" height="120"></canvas>
+                </div>
+            </div>
         </div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    var ctxPekerjaanIbu = document.getElementById('chartPekerjaanIbu').getContext('2d');
+    new Chart(ctxPekerjaanIbu, {
+        type: 'bar',
+        data: {
+            labels: <?= json_encode(array_keys($chart_data_ibu)) ?>,
+            datasets: [{
+                label: 'Jumlah Pendaftar',
+                data: <?= json_encode(array_values($chart_data_ibu)) ?>,
+                backgroundColor: '#00a65a',
+                borderWidth: 0
+            }]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: { display: false },
+                title: {
+                    display: true,
+                    text: 'Grafik Pekerjaan Ibu'
+                }
+            },
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: { precision: 0 }
+                }
+            }
+        }
+    });
+</script>
