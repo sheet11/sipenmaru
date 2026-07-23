7bb28ae99c8ff27b9f26bdec8f4bf201f2cc86d5
sheet <ahmadreynaldo@gmail.com>
2026-07-23 14:22:22 +0700

    feat: add report page for tracking applicant parent occupation statistics by study program

--- admin_sipenmaru/administrator/01_laporan2pil_pekerjaan.php
+++ admin_sipenmaru/administrator/01_laporan2pil_pekerjaan.php
@@ -28,6 +28,48 @@
         <div class="container-fluid" style="margin:10px;">
             <div class="row">
                 <div class="col-md-12">
+                    <h1>Pekerjaan Ayah</h1>
+                </div>
+            </div>
+            <table class="table table-bordered">
+                <tr style="text-align:center">
+                    <th rowspan="2" width="2%">No</th>
+                    <th rowspan="2">Program Studi</th>
+                    <th colspan="<?= count($pekerjaan_list) ?>">Pekerjaan Ayah</th>
+                </tr>
+                <tr style="text-align:center">
+                    <?php foreach ($pekerjaan_list as $pekerjaan): ?>
+                        <th><?= strtoupper($pekerjaan) ?></th>
+                    <?php endforeach; ?>
+                </tr>
+                <?php 
+                $no = 1;
+                foreach ($prodi_list as $label => $prodi_value): ?>
+                <tr>
+                    <td><?= $no++ ?></td>
+                    <td><?= $label ?></td>
+                    <?php foreach ($pekerjaan_list as $pekerjaan): 
+                        if ($prodi_value === "") {
+                            $query = mysqli_query($kon, "SELECT 1 FROM tb_formulir4 WHERE pekerjaan_orang_tua = '$pekerjaan'");
+                        } else {
+                            $query = mysqli_query($kon, "SELECT 1 FROM tb_formulir4 WHERE pilihan_prodi = '$prodi_value' AND pekerjaan_orang_tua = '$pekerjaan'");
+                        }
+                        $jumlah = mysqli_num_rows($query);
+                    ?>
+                        <td><?= $jumlah ?></td>
+                    <?php endforeach; ?>
+                </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
+
+        <br>
+<br>
+<aside class="right-side">
+    <section class="content-header">
+        <div class="container-fluid" style="margin:10px;">
+            <div class="row">
+                <div class="col-md-12">
                     <h1>Pekerjaan Ibu</h1>
                 </div>
             </div>
