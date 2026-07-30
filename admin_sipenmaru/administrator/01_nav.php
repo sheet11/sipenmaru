cb9e03302428334c06981b0e5b9f36cf679f1021
sheet <ahmadreynaldo@gmail.com>
2026-07-30 12:31:47 +0700

    asd

--- admin_sipenmaru/administrator/01_nav.php
+++ admin_sipenmaru/administrator/01_nav.php
@@ -310,7 +310,7 @@ include "session.php";
             <li><a href="importmandiri2pilihan.php"><i class="fa fa-upload"></i> Import Tahap 1</a></li>
 
             <li><a href="importmandiri_2pilihan_tahap2.php"><i class="fa fa-upload"></i> Import Tahap 2</a></li>
-          </ul>
+        </li>
         <li class="treeview"><a href="#"><i class="fa fa-circle-o"></i>Umum<span class="pull-right-container"><i
                 class="fa fa-angle-left pull-right"></i></span>
           </a>
