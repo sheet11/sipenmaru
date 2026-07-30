15f30c82e9c4e90c04ce7050fef6735dcc1d3924
sheet <ahmadreynaldo@gmail.com>
2026-07-30 12:31:12 +0700

    asd

--- admin_sipenmaru/administrator/01_nav.php
+++ admin_sipenmaru/administrator/01_nav.php
@@ -310,26 +310,26 @@ include "session.php";
             <li><a href="importmandiri2pilihan.php"><i class="fa fa-upload"></i> Import Tahap 1</a></li>
 
             <li><a href="importmandiri_2pilihan_tahap2.php"><i class="fa fa-upload"></i> Import Tahap 2</a></li>
-
-            <li class="treeview"><a href="#"><i class="fa fa-circle-o"></i>Umum<span class="pull-right-container"><i
-                    class="fa fa-angle-left pull-right"></i></span>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="01_laporan_1pilihan_gelombang2.php"><i class="fa fa-circle-o"></i>Sudah Membayar</a>
-                </li>
-                <li><a href="01_jenis_kelamin_1pil_mandiri_sb.php"><i class="fa fa-circle-o"></i>Jenis Kelamin</a></li>
-                <li><a href="01_laporan_asal_sekolah_1pil_mandiri_sb.php"><i class="fa fa-circle-o"></i>Asal Sekolah</a>
-                </li>
-                <li><a href="01_laporan_keterangan_sekolah_1pil_mandiri_sb.php"><i class="fa fa-circle-o"></i>Keterangan
-                    Sekolah</a></li>
-                <li><a href="01_laporan1pil_pekerjaan_sb.php"><i class="fa fa-circle-o"></i>Pekerjaan Orang Tua</a>
-                </li>
-                <li><a href="01_laporan1pil_penghasilan_ortu_sb.php"><i class="fa fa-circle-o"></i>Penghasilan Orang
-                    Tua</a>
-                </li>
-              </ul>
+          </ul>
+        <li class="treeview"><a href="#"><i class="fa fa-circle-o"></i>Umum<span class="pull-right-container"><i
+                class="fa fa-angle-left pull-right"></i></span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="01_laporan_1pilihan_gelombang2.php"><i class="fa fa-circle-o"></i>Sudah Membayar</a>
             </li>
+            <li><a href="01_jenis_kelamin_1pil_mandiri_sb.php"><i class="fa fa-circle-o"></i>Jenis Kelamin</a></li>
+            <li><a href="01_laporan_asal_sekolah_1pil_mandiri_sb.php"><i class="fa fa-circle-o"></i>Asal Sekolah</a>
+            </li>
+            <li><a href="01_laporan_keterangan_sekolah_1pil_mandiri_sb.php"><i class="fa fa-circle-o"></i>Keterangan
+                Sekolah</a></li>
+            <li><a href="01_laporan1pil_pekerjaan_sb.php"><i class="fa fa-circle-o"></i>Pekerjaan Orang Tua</a>
+            </li>
+            <li><a href="01_laporan1pil_penghasilan_ortu_sb.php"><i class="fa fa-circle-o"></i>Penghasilan Orang
+                Tua</a>
+            </li>
+          </ul>
         </li>
+
         </ul>
 
         </li>
